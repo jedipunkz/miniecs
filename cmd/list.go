693070f2c9062759b890150d69e75abe9f8033d9
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2022-11-06 13:47:16 +0900

    add option to specify cluster

--- cmd/list.go
+++ cmd/list.go
@@ -13,7 +13,8 @@ import (
 )
 
 var listSetFlags struct {
-	region string
+	region  string
+	cluster string
 }
 
 // listCmd represents the listcommand
@@ -33,6 +34,25 @@ var listCmd = &cobra.Command{
 
 		var ecsMatrix [][]string
 
+		ecsMatrix = getECSMatrix(e)
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{
+			"Cluster Name",
+			"Service Name",
+			"TaskDefinition Name",
+			"Container Name"})
+		table.SetFooter([]string{"Total", "", "", strconv.Itoa(len(ecsMatrix))}) // Add Footer
+		table.SetBorder(true)
+		table.AppendBulk(ecsMatrix)
+		table.Render()
+	},
+}
+
+func getECSMatrix(e *myecs.ECS) [][]string {
+	var ecsMatrix [][]string
+
+	if listSetFlags.cluster == "" {
 		for _, cluster := range e.Clusters {
 			if err := e.ListServices(cluster); err != nil {
 				log.Fatal(err)
@@ -50,24 +70,34 @@ var listCmd = &cobra.Command{
 				}
 			}
 		}
+	} else {
+		if err := e.ListServices(listSetFlags.cluster); err != nil {
+			log.Fatal(err)
+		}
+		for _, service := range e.Services {
+			if err := e.GetTaskDefinition(listSetFlags.cluster, service); err != nil {
+				log.Fatal(err)
+			}
+			if err := e.GetContainerName(e.TaskDefinition); err != nil {
+				log.Fatal(err)
+			}
+			for i := range e.Containers {
+				ecsMatrix = append(ecsMatrix,
+					[]string{listSetFlags.cluster, service, e.TaskDefinition, e.Containers[i]})
+			}
+		}
+	}
 
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{
-			"Cluster Name",
-			"Service Name",
-			"TaskDefinition Name",
-			"Container Name"})
-		table.SetFooter([]string{"Total", "", "", strconv.Itoa(len(ecsMatrix))}) // Add Footer
-		table.SetBorder(true)
-		table.AppendBulk(ecsMatrix)
-		table.Render()
-	},
+	return ecsMatrix
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
 	listCmd.Flags().StringVarP(&listSetFlags.region, "region", "", "", "Region Name")
 	if err := listCmd.MarkFlagRequired("region"); err != nil {
 		log.Fatal(err)
 	}
+
+	listCmd.Flags().StringVarP(&listSetFlags.cluster, "cluster", "", "", "ECS Cluster Name")
 }
