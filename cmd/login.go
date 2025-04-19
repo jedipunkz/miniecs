ab07b0be754011784505516beda31004aaacb570
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2025-04-19 20:57:43 +0900

    refactor: login.go

--- cmd/login.go
+++ cmd/login.go
@@ -13,11 +13,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var loginSetFlags struct {
+type loginFlags struct {
 	region  string
 	cluster string
 	shell   string
 }
+
+var loginSetFlags loginFlags
 
 var loginCmd = &cobra.Command{
 	Use:   "login",
@@ -28,68 +30,111 @@ var loginCmd = &cobra.Command{
 func runLoginCmd(cmd *cobra.Command, args []string) {
 	ctx := context.Background()
 
-	var ecsResources []myecs.ECSResource
-
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(loginSetFlags.region))
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
-
-	if os.Getenv("AWS_PROFILE") == "" {
-		log.Fatal("set AWS_PROFILE environment variable to use")
-	}
-
-	e := myecs.NewEcs(cfg, loginSetFlags.region)
-	if e == nil {
-		log.Fatal("failed to initialize ECS client")
-	}
-
-	err = e.ListClusters(ctx)
+	ecsClient, err := initializeECSClient(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, cluster := range e.Clusters {
-		err = e.ListServices(ctx, cluster.ClusterName)
-		if err != nil {
-			log.Fatal(err)
-		}
+	ecsResources, err := collectECSResources(ctx, ecsClient)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		for _, service := range e.Services {
-			err := e.GetTasks(ctx, cluster.ClusterName, service.ServiceName)
-			if err != nil {
-				log.Fatal(err)
-			}
+	selectedIndices, err := selectECSResource(ecsResources)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-			if len(e.Tasks) > 0 {
-				task := e.Tasks[0] // set first task
+	if err := executeLogin(ecsClient, selectedIndices, ecsResources); err != nil {
+		log.Fatal(err)
+	}
+}
 
-				e.Containers = nil
+func initializeECSClient(ctx context.Context) (*myecs.ECSResource, error) {
+	if os.Getenv("AWS_PROFILE") == "" {
+		return nil, fmt.Errorf("set AWS_PROFILE environment variable to use")
+	}
 
-				err := e.ListContainers(ctx, task.TaskDefinition)
-				if err != nil {
-					log.Fatal(err)
-				}
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(loginSetFlags.region))
+	if err != nil {
+		return nil, fmt.Errorf("unable to load SDK config: %w", err)
+	}
 
-				for _, container := range e.Containers {
-					ecsResource := myecs.ECSResource{
-						Clusters:   []myecs.Cluster{cluster},
-						Services:   []myecs.Service{service},
-						Tasks:      []myecs.Task{task},
-						Containers: []myecs.Container{container},
-					}
-					ecsResources = append(ecsResources, ecsResource)
-				}
-			}
+	ecsClient := myecs.NewEcs(cfg, loginSetFlags.region)
+	if ecsClient == nil {
+		return nil, fmt.Errorf("failed to initialize ECS client")
+	}
+
+	return ecsClient, nil
+}
+
+func collectECSResources(ctx context.Context, ecsClient *myecs.ECSResource) ([]myecs.ECSResource, error) {
+	var ecsResources []myecs.ECSResource
+
+	if err := ecsClient.ListClusters(ctx); err != nil {
+		return nil, fmt.Errorf("failed to list clusters: %w", err)
+	}
+
+	for _, cluster := range ecsClient.Clusters {
+		if err := collectServicesAndContainers(ctx, ecsClient, cluster, &ecsResources); err != nil {
+			return nil, err
 		}
 	}
 
-	idx, err := fuzzyfinder.FindMulti(
+	return ecsResources, nil
+}
+
+func collectServicesAndContainers(ctx context.Context, ecsClient *myecs.ECSResource, cluster myecs.Cluster, ecsResources *[]myecs.ECSResource) error {
+	if err := ecsClient.ListServices(ctx, cluster.ClusterName); err != nil {
+		return fmt.Errorf("failed to list services: %w", err)
+	}
+
+	for _, service := range ecsClient.Services {
+		if err := collectTasksAndContainers(ctx, ecsClient, cluster, service, ecsResources); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func collectTasksAndContainers(ctx context.Context, ecsClient *myecs.ECSResource, cluster myecs.Cluster, service myecs.Service, ecsResources *[]myecs.ECSResource) error {
+	if err := ecsClient.GetTasks(ctx, cluster.ClusterName, service.ServiceName); err != nil {
+		return fmt.Errorf("failed to get tasks: %w", err)
+	}
+
+	if len(ecsClient.Tasks) == 0 {
+		return nil
+	}
+
+	task := ecsClient.Tasks[0]
+	ecsClient.Containers = nil
+
+	if err := ecsClient.ListContainers(ctx, task.TaskDefinition); err != nil {
+		return fmt.Errorf("failed to list containers: %w", err)
+	}
+
+	for _, container := range ecsClient.Containers {
+		*ecsResources = append(*ecsResources, myecs.ECSResource{
+			Clusters:   []myecs.Cluster{cluster},
+			Services:   []myecs.Service{service},
+			Tasks:      []myecs.Task{task},
+			Containers: []myecs.Container{container},
+		})
+	}
+
+	return nil
+}
+
+func selectECSResource(ecsResources []myecs.ECSResource) ([]int, error) {
+	return fuzzyfinder.FindMulti(
 		ecsResources,
 		func(i int) string {
-			return ecsResources[i].Clusters[0].ClusterName + " " +
-				ecsResources[i].Services[0].ServiceName + " " +
-				ecsResources[i].Containers[0].ContainerName
+			return fmt.Sprintf("%s %s %s",
+				ecsResources[i].Clusters[0].ClusterName,
+				ecsResources[i].Services[0].ServiceName,
+				ecsResources[i].Containers[0].ContainerName,
+			)
 		},
 		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
 			if i == -1 {
@@ -103,41 +148,34 @@ func runLoginCmd(cmd *cobra.Command, args []string) {
 			)
 		}),
 	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := login(e, idx, ecsResources); err != nil {
-		log.Fatal(err)
-	}
 }
 
-func login(e *myecs.ECSResource, idx []int, myecs []myecs.ECSResource) error {
-	in := ecs.ExecuteCommandInput{}
-	in.Cluster = &myecs[idx[0]].Clusters[0].ClusterName
-	in.Container = &myecs[idx[0]].Containers[0].ContainerName
-	in.Task = &myecs[idx[0]].Tasks[0].TaskArn
-
-	if loginSetFlags.shell != "" {
-		in.Command = &loginSetFlags.shell
-	} else {
-		defaultShell := "sh"
-		in.Command = &defaultShell
-	}
+func executeLogin(ecsClient *myecs.ECSResource, selectedIndices []int, ecsResources []myecs.ECSResource) error {
+	selectedResource := ecsResources[selectedIndices[0]]
+	commandInput := createExecuteCommandInput(selectedResource)
 
 	log.WithFields(log.Fields{
-		"cluster":   *in.Cluster,
-		"task":      *in.Task,
-		"container": *in.Container,
-		"command":   *in.Command,
+		"cluster":   *commandInput.Cluster,
+		"task":      *commandInput.Task,
+		"container": *commandInput.Container,
+		"command":   *commandInput.Command,
 	}).Info("ECS Execute Login with These Parameters")
 
-	if err := e.ExecuteCommand(in); err != nil {
-		log.Fatalf("failed to execute command: %v", err)
-		return err
+	return ecsClient.ExecuteCommand(commandInput)
+}
+
+func createExecuteCommandInput(resource myecs.ECSResource) ecs.ExecuteCommandInput {
+	shell := loginSetFlags.shell
+	if shell == "" {
+		shell = "sh"
 	}
 
-	return nil
+	return ecs.ExecuteCommandInput{
+		Cluster:   &resource.Clusters[0].ClusterName,
+		Container: &resource.Containers[0].ContainerName,
+		Task:      &resource.Tasks[0].TaskArn,
+		Command:   &shell,
+	}
 }
 
 func init() {
